40eed98e002718e8be07c4f642e3a51da86057fe
Kosbys <kosmicabysslord@protonmail.com>
2026-09-09 12:16:45 +0300

    Add axum with psql

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # fakefood
+
+
+# Backend flow:
+
+User adds item to cart -> send post request -> save cart items in state -> user buys -> save purchase information in database ->
+-> response can be a websocket connection that tracks the order until its finished
--- api/src/main.rs
+++ api/src/main.rs
@@ -1,10 +1,22 @@
+mod db;
+mod routes;
+
+use crate::db::create_pool;
 use axum::{Router, routing::get};
+use dotenv::dotenv;
+use std::env;
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
+
+    let db_url = env::var("database_string").unwrap();
+
+    let database = create_pool(&db_url).await;
+
     let app = Router::new().route("/", get(|| async { "Hello" }));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    println!("Workikng");
+    println!("Working");
     axum::serve(listener, app).await.unwrap();
 }
