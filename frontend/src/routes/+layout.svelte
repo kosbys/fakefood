5b440700fbd41002b70f66b6c94caad4be6398bd
Hiro <84511935+Hiro420@users.noreply.github.com>
2026-09-23 10:58:52 +0300

    small

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import favicon from "$lib/assets/favicon.svg";
+    import Header from "$lib/components/header.svelte";
     import "../app.css"
 
     let { children } = $props();
@@ -9,4 +10,5 @@
     <link rel="icon" href={favicon} />
 </svelte:head>
 
+<Header/>
 {@render children()}
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import Product from "$lib/components/product.svelte";
+</script>
+
+
+<div class="flex items-center justify-center">
+  <Product />
+</div>
